f3926818ae70558a009ad28ce62d7ae670cabcc4
chelizichen <1347290221@qq.com>
2024-04-13 19:18:41 +0800

    build

--- build.local.sh
+++ build.local.sh
@@ -3,12 +3,12 @@
 # if permission denied
 # run script with ` chmod +x build.sh ` 
 readonly ServerName="ChatServer"
-
+readonly SgridFile="sgrid_app"
 # rm
-rm ./$ServerName.tar.gz ./service_go
+rm ./$ServerName.tar.gz ./SgridFile
 
 # compile
-go build -o service_go
+go build -o $SgridFile
 
 # build
-tar -cvf $ServerName.tar.gz ./simp.yaml ./service_go ./dist
+tar -cvf $ServerName.tar.gz ./sgrid.yaml ./$SgridFile
